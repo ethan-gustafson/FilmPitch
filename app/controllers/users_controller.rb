ca7ca253f812c48ecaa4d2130486f425a9d23297
Ethan <ethan_gustafson@outlook.com>
2020-02-11 19:22:52 -0500

    added root route, users resources for [:new, :create], added users controller 'home', 'new', 'create' with private 'user_params + require login before_action. Created logged_in & current_user helper method and added protect_from_forgery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,38 @@
 class UsersController < ApplicationController
+    before_action :require_login
+
+    def home
+
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            redirect_to new_user_path
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :name, 
+            :username, 
+            :email, 
+            :password, 
+            :password_confirmation
+        )
+    end
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
+    validates :name, :username, :email, presence: true
+    validates :username, :email, uniqueness: true
     has_many :funds
 	has_many :funded_pitches, through: :funds, source: :pitch
 	has_many :pitches
