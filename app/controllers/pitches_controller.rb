97cb8af6e9ceafadb1625ead00f418971a992be4
Ethan <ethan_gustafson@outlook.com>
2020-02-12 01:56:48 -0500

    added form for new pitch, plus create action logic, with redirects

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -5,11 +5,16 @@ class PitchesController < ApplicationController
     end
 
     def new
-
+        @pitch = Pitch.new
     end
 
     def create
-
+        @pitch = Pitch.new(pitch_params)
+        if @pitch.save
+            redirect_to user_pitch_path(@pitch.user, @pitch)
+        else
+            redirect_to new_user_pitch_path
+        end
     end
 
     def show
