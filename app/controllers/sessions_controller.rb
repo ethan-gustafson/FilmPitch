e66ee0274a9f1e6010dd87cce5c9b355f68e0da2
Ethan <ethan_gustafson@outlook.com>
2020-02-11 20:29:35 -0500

    added secret key base, login, signup and logout functional

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(name: params[:user][:username])
+        @user = User.find_by(username: params[:user][:username])
         if @user.try(:authenticate, params[:user][:password])
             session[:user_id] = @user.id
             redirect_to root_path
