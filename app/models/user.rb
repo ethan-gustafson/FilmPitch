50dc4a43f6ebf161efa4769981c59a24e66b3e04
Ethan <ethan_gustafson@outlook.com>
2020-02-12 13:18:44 -0500

    placed require login method into application controller. put validations on wallet so that it can't be less than 0 or greater than 1000.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :name, :username, :email, presence: true
     validates :username, :email, uniqueness: true
-    validates(:wallet, { length: {minimum: 0, maximum: 1000} })
+    validates :wallet, numericality: {greater_than: 0, less_than_or_equal_to: 1000, message: "You cannot have more than 1000 in your account" }
     has_many :funds
 	has_many :funded_pitches, through: :funds, source: :pitch
     has_many :pitches
     
     def transaction(dollars)
         remaining_balance = self.wallet - dollars 
-        self.update_attributes wallet: remaining_balance
+        self.update_attributes! wallet: remaining_balance
         self.wallet
     end
 
