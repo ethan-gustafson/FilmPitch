761367d3c39c23a24a1e681ff5122a23d1fd1141
Ethan <ethan_gustafson@outlook.com>
2020-02-12 07:58:23 -0500

    User bank -> Funding all functional, funding has validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :name, :username, :email, presence: true
     validates :username, :email, uniqueness: true
+    validates(:wallet, { length: {minimum: 0, maximum: 1000} })
     has_many :funds
 	has_many :funded_pitches, through: :funds, source: :pitch
     has_many :pitches
