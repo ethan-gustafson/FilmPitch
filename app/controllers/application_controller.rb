f95c1a479fcc3f2a3d9f237d4d916fcaa1541fde
Ethan <ethan_gustafson@outlook.com>
2020-02-11 19:22:52 -0500

    added root route, users resources for [:new, :create], added users controller 'home', 'new', 'create' with private 'user_params + require login before_action. Created logged_in & current_user helper method and added protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception #csrf
+    helper_method :logged_in
+    helper_method :current_user
+    
+    def logged_in
+        if !session[:user_id]
+            redirect_to new_user_path
+        end
+    end
+
+    def current_user
+        User.find(session[:user_id])
+    end
 end
