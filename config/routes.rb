4e8b4d8398792709baa72c45f08ddd82dfc7ab44
Ethan <ethan_gustafson@outlook.com>
2020-02-12 17:36:14 -0500

    user signup displaying validations functional

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'users#home'
   get '/signup', to: 'users#new', as: "new_user"
+  post '/signup', to: 'users#create', as: 'users_path'
   get '/login', to: 'sessions#new' 
   post '/login', to: 'sessions#create' 
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users, only: [:create, :show, :edit, :update] do
+  resources :users, only: [:show, :edit, :update] do
     resources :pitches, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   end
 
