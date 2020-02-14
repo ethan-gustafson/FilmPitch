f7595ff68e6a481267eb443aca1deccdd797e62f
Ethan <ethan_gustafson@outlook.com>
2020-02-14 03:38:35 -0500

    added omniauth link to login/signup page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,4 +18,21 @@ class SessionsController < ApplicationController
         redirect_to new_user_path
     end
 
+    def omniauth
+        # @user = User.find_or_create_by(uid: auth[:uid]) do |u|
+        #     u.username= auth[:info][:username]
+        #     u.email= auth[:info][:email]
+        #     u.password= SecureRandom.hex
+    #end
+    #session[:user_id] = @user.id
+    #redirect_to user_path(@user)
+        #byebug
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
+
 end
