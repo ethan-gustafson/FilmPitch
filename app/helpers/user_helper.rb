97c26c1151479017f1fc6a9634f68715fa4de4cf
Ethan <ethan_gustafson@outlook.com>
2020-02-15 20:08:46 -0500

    routes are now protected for missing users

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,7 +1,11 @@
 module UserHelper
 
     def set_user
-        @user = User.find(params[:id])
+        if User.find_by_id(params[:id])
+            @user = User.find(params[:id])
+        else
+            redirect_to pitches_path
+        end
     end
 
 end
