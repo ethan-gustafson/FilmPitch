e1d01271520ed16eafa64cf024eb7dc19ecda2b4
Ethan <ethan_gustafson@outlook.com>
2020-04-21 03:04:48 -0400

    removed has_many: :through relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
     has_secure_password
+
+    has_many :funds
+    has_many :pitches
+    
     validates :name, :username, :email, presence: true
     validates :username, :email, uniqueness: true
     validates :wallet, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 1000, message: "You cannot have more than 1000 in your account" }
-    has_many :funds
-	has_many :funded_pitches, through: :funds, source: :pitch
-    has_many :pitches
     
     def transaction(dollars)
         remaining_balance = self.wallet - dollars 
