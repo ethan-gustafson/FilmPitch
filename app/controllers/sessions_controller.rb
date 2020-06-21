3827f9bdf01ba778f5d77551fab05c0a21c36ffe
Ethan <ethan_gustafson@outlook.com>
2020-06-21 03:06:03 -0400

    when a user types invalid login, the invalid login notice will render on screen

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,13 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to root_path
         else
-            redirect_to new_user_path # if the password fails redirect to the signup page
+            redirect_to login_path, :notice => "Invalid Login" # if the password fails redirect to the login page
         end
     end
 
     def destroy
         session.delete :user_id
-        redirect_to new_user_path
+        redirect_to login_path
     end
 
     def omniauth
