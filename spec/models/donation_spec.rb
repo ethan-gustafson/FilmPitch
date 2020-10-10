90b1c8646b127a745c9368ada163b47e84bf6175
Ethan <ethan_gustafson@outlook.com>
2020-10-10 01:24:46 -0400

    generated donations controller

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -8,12 +8,12 @@ RSpec.describe Donation, type: :model do
     expect(subject.amount).to eq(100.00) 
   end
 
-  xit "belongs to a user" do
+  it "belongs to a user" do
     allow(subject).to receive(:user_id).and_return(1)
     expect(subject.user_id).to eq(1) 
   end
 
-  xit "belongs to a project" do 
+  it "belongs to a project" do 
     allow(subject).to receive(:project_id).and_return(1)
     expect(subject.project_id).to eq(1) 
   end
