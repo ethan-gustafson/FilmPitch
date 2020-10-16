02bee1d3063dd6f631b6b868948ff05fcc84f1ab
Ethan <ethan_gustafson@outlook.com>
2020-10-16 01:18:35 -0400

    users can now upload their own profile image when updating their account

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :profile_image])
   end
 
   # The path used after sign up.
