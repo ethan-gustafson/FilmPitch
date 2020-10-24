13935def7ba6a0b682f7fa70fd20aa2231331023
Ethan <ethan_gustafson@outlook.com>
2020-10-23 22:31:42 -0400

    change project find by attrib method to be dynamic

--- app/models/project.rb
+++ app/models/project.rb
@@ -15,9 +15,9 @@ class Project < ApplicationRecord
   @@genres = {
     :action=>"Action", :adventure=>"Adventure", biopic: "Biopic", 
     :comedy=>"Comedy", :crime=>"Crime", :drama=>"Drama", :fantasy=>"Fantasy", 
-    :historical=>"Historical", :horror=>"Horror", :mystery=>"Mystery", noir: "Noir" 
+    :historical=>"Historical", :horror=>"Horror", :mystery=>"Mystery", noir: "Noir", 
     :philosophical=>"Philosophical", :political=>"Political", :romance=>"Romance", 
-    :satire=>"Satire", :"science fiction"=>"Science Fiction", :silent=>"Silent", 
+    :satire=>"Satire", :science_fiction=>"Science Fiction", :silent=>"Silent", 
     :social=>"Social", :thriller=>"Thriller", :urban=>"Urban", :western=>"Western"
   }
 
@@ -44,8 +44,10 @@ class Project < ApplicationRecord
     @@genres
   end
 
-  def self.find_projects_by_genre(genre)
-    where(genre: genre.capitalize).last(10)
+  def self.find_projects_by_attr(key:, value:)
+    # key = :genre, value = "Comedy"
+    # where(:genre => "Comedy").last(10)
+    where(key => value).last(10)
   end
 
   def acceptable_genre?(genre)
