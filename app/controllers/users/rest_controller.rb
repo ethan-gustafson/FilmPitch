f88d672040e22f12c31ae8e0a563a0b0d8d4d340
Ethan <ethan_gustafson@outlook.com>
2020-10-24 20:47:43 -0400

    set dynamic user show page with email present

--- app/controllers/users/rest_controller.rb
+++ app/controllers/users/rest_controller.rb
@@ -1,4 +1,9 @@
 class Users::RestController < ApplicationController
   def show
+    @user = User.find_by_id(params[:id])
+
+    if @user.nil?
+      redirect_to user_path(current_user)
+    end
   end
 end
