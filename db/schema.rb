054d7f28220f03855026c08c4f509873581c4bf5
Ethan <ethan_gustafson@outlook.com>
2020-10-29 00:30:44 -0400

    Revert "add migration changing amount data type to integer & migrate"
    This reverts commit 3239767a6dbf8dcbc2f0ab1b838db9b903ba3a4e.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_042449) do
+ActiveRecord::Schema.define(version: 2020_10_10_170458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_10_29_042449) do
   end
 
   create_table "donations", force: :cascade do |t|
-    t.integer "amount"
+    t.decimal "amount"
     t.integer "user_id"
     t.integer "project_id"
     t.datetime "created_at", precision: 6, null: false
