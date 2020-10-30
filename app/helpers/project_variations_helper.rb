823398391d4919cbb723ddc24b5955ffdece6f5b
Ethan <ethan_gustafson@outlook.com>
2020-10-30 09:36:15 -0400

    create project_variations_helper, add project controller private methods

---
+++ app/helpers/project_variations_helper.rb
@@ -0,0 +1,2 @@
+module ProjectVariationsHelper
+end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,17 @@
 module ProjectsHelper
+
+  def find_project_by_id
+    @project = Project.find_by_id(params[:id])
+     
+    redirect_to projects_path if @project.nil?
+  end
+
+  def show_comments?
+    # If the project exits == If the project isn't nil
+    if !@project.nil?
+      # Eager load the project comments, including the user data
+      @comments = @project.comments.includes(:user).last(10).reverse
+    end
+  end
+  
 end
