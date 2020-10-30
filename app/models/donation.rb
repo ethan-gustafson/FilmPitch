4fef6ef77ffe7fa8f4d1a2fc328000bab1381359
Ethan <ethan_gustafson@outlook.com>
2020-10-30 09:38:34 -0400

    add associations, monetize amount_cents in donation.rb

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,11 @@
 class Donation < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  monetize :amount_cents,
+    allow_nil: false, 
+    numericality: { 
+      greater_than_or_equal_to: 0, 
+      less_than_or_equal_to: 1000000
+    }
 end
