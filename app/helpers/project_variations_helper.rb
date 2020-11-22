90fa863670f1551a2cbfde4ad71a2c36c8a90d72
Ethan <ethan_gustafson@outlook.com>
2020-11-22 12:11:34 -0500

    move film_types and genres methods to their respective helpers

--- app/helpers/project_variations_helper.rb
+++ app/helpers/project_variations_helper.rb
@@ -1,2 +1,12 @@
 module ProjectVariationsHelper
+  def film_types
+    {
+      advertisement: "Advertisement", 
+      animation: "Animation", 
+      documentary: "Documentary", 
+      feature: "Feature", 
+      short: "Short",
+      episodic: "Episodic"
+    }
+  end
 end
