0e035bd49ee71d714f17233a2a15f07df398b220
Ethan <ethan_gustafson@outlook.com>
2020-11-22 12:13:44 -0500

    add current_user_project?, show_comments? methods to projects_helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,16 +2,14 @@ module ProjectsHelper
 
   def find_project_by_id
     @project = Project.find_by_id(params[:id])
-     
     redirect_to projects_path if @project.nil?
   end
 
-  def show_comments?
-    # If the project exits == If the project isn't nil
-    if !@project.nil?
-      # Eager load the project comments, including the user data
-      @comments = @project.comments.includes(:user).last(10).reverse
-    end
+  def current_user_project?
+    current_user.id.equal?(@project.user_id)
   end
-  
+
+  def show_comments?
+    @comments = @project.comments.includes(:user).last(10).reverse if !@project.nil?
+  end
 end
