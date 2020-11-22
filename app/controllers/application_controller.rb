1f82e087f1c3c0a61f8a15301c7189d45a959435
Ethan <ethan_gustafson@outlook.com>
2020-11-22 12:09:51 -0500

    include remaining modules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+  include ProjectVariationsHelper
   include ProjectsHelper
+  include GenresHelper
+  include DonationsHelper
+  include CommentsHelper
 
   # protect_from_forgery(options = {})
   # Turn on request forgery protection. Bear in mind that GET and HEAD requests are not checked.
@@ -9,5 +13,4 @@ class ApplicationController < ActionController::Base
   # :exception - Raises ActionController::InvalidAuthenticityToken exception.
 
   protect_from_forgery with: :exception
-
 end
