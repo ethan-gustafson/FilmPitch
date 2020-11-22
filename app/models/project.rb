ff13a728bed526caa369ff8a99890ce46cd821d0
Ethan <ethan_gustafson@outlook.com>
2020-11-22 12:06:21 -0500

    change film_types method, rearrange queries, remove Project.film_types & .genres methods

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,10 +8,9 @@ class Project < ApplicationRecord
   # before_destroy -> When donations are created, projects will no longer be allowed
   # to be destroyed if there are donations on the project.
 
+  belongs_to :user
   has_one_attached :cover_image
   has_one_attached :script
-  belongs_to :user
-
   has_many :comments
   has_many :donations
 
@@ -20,51 +19,25 @@ class Project < ApplicationRecord
     less_than_or_equal_to: 1000000
   }
 
-  # ----------------
-
-  scope :newly_created, -> { order(created_at: :desc) }
-  scope :fully_funded, -> { includes(:user).where(goal_cents: 0) }
-
-  # ----------------
-
-  @@genres = {
-    :action=>"Action", :adventure=>"Adventure", biopic: "Biopic", 
-    :comedy=>"Comedy", :crime=>"Crime", :drama=>"Drama", :fantasy=>"Fantasy", 
-    :historical=>"Historical", :horror=>"Horror", :mystery=>"Mystery", noir: "Noir", 
-    :philosophical=>"Philosophical", :political=>"Political", :romance=>"Romance", 
-    :satire=>"Satire", :science_fiction=>"Science Fiction", :silent=>"Silent", 
-    :social=>"Social", :thriller=>"Thriller", :urban=>"Urban", :western=>"Western"
-  }
-
-  @@film_types = {
-    advertisement: "Advertisement", animation: "Animation", 
-    documentary: "Documentary", feature: "Feature", short: "Short",
-    episodic: "Episodic"
-  }
-
-  def self.genres
-    @@genres
-  end
-
-  def self.film_types
-    @@film_types
-  end
-
   def acceptable_film_type?
-    if !@@film_types.has_key?(self.film_type.downcase.to_sym) 
+    if !film_types.has_key?(self.film_type.downcase.to_sym) 
       errors.add(:film_type, "invalid type")
     end
   end
 
   def acceptable_genre?
-    if !@@genres.has_key?(self.genre.downcase.to_sym) 
+    if !genres.has_key?(self.genre.downcase.to_sym) 
       errors.add(:genre, "invalid category")
     end
   end
 
+  scope :newly_created, -> { order(created_at: :desc) }
+  scope :fully_funded, -> { includes(:user).where(goal_cents: 0) }
+
   def self.most_popular
-    joins(:comments).select(
-      "projects.*, count(comments.id) AS comments_count"
-    ).group("projects.id").order("comments_count DESC")
+    joins(:comments)
+    .select("projects.*, count(comments.id) AS comments_count")
+    .group("projects.id").
+    order("comments_count DESC")
   end
 end
