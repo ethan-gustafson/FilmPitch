5e46da386e0870729880a695a525d3417942e667
Ethan <ethan_gustafson@outlook.com>
2020-11-27 19:28:00 -0500

    remove genres & film_types methods in genres/project_variations_helper

--- app/helpers/genres_helper.rb
+++ app/helpers/genres_helper.rb
@@ -1,27 +1,2 @@
 module GenresHelper
-  def genres
-    {
-      action: "Action", 
-      adventure: "Adventure", 
-      biopic: "Biopic", 
-      comedy: "Comedy", 
-      crime: "Crime",   
-      drama: "Drama", 
-      fantasy: "Fantasy", 
-      historical: "Historical", 
-      horror: "Horror", 
-      mystery: "Mystery", 
-      noir: "Noir", 
-      philosophical: "Philosophical", 
-      political: "Political", 
-      romance: "Romance", 
-      satire: "Satire", 
-      science_fiction: "Science Fiction", 
-      silent: "Silent", 
-      social: "Social", 
-      thriller: "Thriller", 
-      urban: "Urban", 
-      western: "Western"
-    }
-  end
 end
--- app/helpers/project_variations_helper.rb
+++ app/helpers/project_variations_helper.rb
@@ -1,12 +1,2 @@
 module ProjectVariationsHelper
-  def film_types
-    {
-      advertisement: "Advertisement", 
-      animation: "Animation", 
-      documentary: "Documentary", 
-      feature: "Feature", 
-      short: "Short",
-      episodic: "Episodic"
-    }
-  end
 end
