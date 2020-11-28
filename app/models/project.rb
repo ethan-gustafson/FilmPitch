e0f76781eb054e8765f48d4dbb9d7c3420fd8632
Ethan <ethan_gustafson@outlook.com>
2020-11-27 19:28:35 -0500

    add genres, film_types methods, add donations before_destroy callback validation method

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,7 @@ class Project < ApplicationRecord
   validates :goal, numericality: { greater_than_or_equal_to: 0 }
 
   validate :acceptable_genre?, :acceptable_film_type?
+  before_destroy :donations_prevents_deletion?
 
   # before_destroy -> When donations are created, projects will no longer be allowed
   # to be destroyed if there are donations on the project.
@@ -19,16 +20,27 @@ class Project < ApplicationRecord
     less_than_or_equal_to: 1000000
   }
 
-  def acceptable_film_type?
-    if !film_types.has_key?(self.film_type.downcase.to_sym) 
-      errors.add(:film_type, "invalid type")
-    end
+  @@genres = {
+    :action=>"Action", :adventure=>"Adventure", biopic: "Biopic", 
+    :comedy=>"Comedy", :crime=>"Crime", :drama=>"Drama", :fantasy=>"Fantasy", 
+    :historical=>"Historical", :horror=>"Horror", :mystery=>"Mystery", noir: "Noir", 
+    :philosophical=>"Philosophical", :political=>"Political", :romance=>"Romance", 
+    :satire=>"Satire", :science_fiction=>"Science Fiction", :silent=>"Silent", 
+    :social=>"Social", :thriller=>"Thriller", :urban=>"Urban", :western=>"Western"
+  }
+
+  @@film_types = {
+    advertisement: "Advertisement", animation: "Animation", 
+    documentary: "Documentary", feature: "Feature", short: "Short",
+    episodic: "Episodic"
+  }
+
+  def self.genres
+    @@genres
   end
 
-  def acceptable_genre?
-    if !genres.has_key?(self.genre.downcase.to_sym) 
-      errors.add(:genre, "invalid category")
-    end
+  def self.film_types
+    @@film_types
   end
 
   scope :newly_created, -> { order(created_at: :desc) }
@@ -39,5 +51,23 @@ class Project < ApplicationRecord
     .select("projects.*, count(comments.id) AS comments_count")
     .group("projects.id").
     order("comments_count DESC")
+  end
+
+  def acceptable_film_type?
+    if !@@film_types.has_key?(self.film_type.downcase.to_sym) 
+      errors.add(:film_type, "invalid type")
+    end
+  end
+
+  def acceptable_genre?
+    if !@@genres.has_key?(self.genre.downcase.to_sym) 
+      errors.add(:genre, "invalid category")
+    end
+  end
+
+  def donations_prevents_deletion?
+    if self.donations.count > 0
+      errors.add(:goal_cents, "Project cannot be deleted due to donations")
+    end
   end
 end
